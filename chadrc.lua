3c64a57911a526cb7110a1e1b74b4384ca8cb85c
xingzhe <xingzhe@micous.com>
2023-12-29 18:25:48 +0800

    commit

--- chadrc.lua
+++ chadrc.lua
@@ -6,10 +6,17 @@ local highlights = require "custom.highlights"
 
 M.ui = {
   theme = "monekai",
-  theme_toggle = { "monekai", "one_light" },
+  theme_toggle = { "monekai" },
 
   hl_override = highlights.override,
   hl_add = highlights.add,
+
+  tabufline = {
+    enabled = false,
+  },
+  statusline = {
+    theme = "default",
+  }
 }
 
 M.plugins = "custom.plugins"
