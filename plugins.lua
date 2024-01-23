252d5565b68be0518cecd0e7b3ebc0a8e489be6c
xingzhe <xingzhe@micous.com>
2024-01-23 19:39:50 +0800

    禁止使用NvChad/nvim-colorizer.lua

--- plugins.lua
+++ plugins.lua
@@ -110,12 +110,10 @@ local plugins = {
       require("marks").setup()
     end,
   },
-  -- To make a plugin not be loaded
-  -- {
-  --   "NvChad/nvim-colorizer.lua",
-  --   enabled = false
-  -- },
-
+  {
+    "NvChad/nvim-colorizer.lua",
+    enabled = false,
+  },
   -- All NvChad plugins are lazy-loaded by default
   -- For a plugin to be loaded, you will need to set either `ft`, `cmd`, `keys`, `event`, or set `lazy = false`
   -- If you want a plugin to load on startup, add `lazy = false` to a plugin spec, for example
