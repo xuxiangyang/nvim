7ec29ebef5cc3ac864afd82c6ac40aac0743c2b2
xingzhe <xingzhe@micous.com>
2024-01-29 11:20:18 +0800

    [update] 禁止Lazy自动检测配置变化

--- chadrc.lua
+++ chadrc.lua
@@ -29,4 +29,11 @@ M.plugins = "custom.plugins"
 -- check core.mappings for table structure
 M.mappings = require "custom.mappings"
 
+M.lazy_nvim = {
+  change_detection = {
+    enabled = false,
+    notify = false,
+  },
+}
+
 return M
